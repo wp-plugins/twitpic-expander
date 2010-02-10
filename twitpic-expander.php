e304b6623b6890867af57a3ce646030f1c53d018
bh <bh@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-02-10 22:08:18 +0000

    tagging 1.1
    git-svn-id: https://plugins.svn.wordpress.org/twitpic-expander/tags/1.1@203976 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- twitpic-expander.php
+++ twitpic-expander.php
@@ -3,28 +3,28 @@
 Plugin Name: Twitpic Expander
 Plugin URI: http://www.jacksonlaycock.com/
 Description: Scans posts for twitpic URL's and automatically expands them and shows an image.  
-Version: 1.0
+Version: 1.1
 Author: Jackson Laycock
 Author URI: http://www.jacksonlaycock.com
 */
 ?>
-<?php
-// borrowed code from http://code.google.com/p/dabr/source/browse/trunk/common/twitter.php?spec=svn116&r=116
 
-function showtwitpic($text){ 
-	$foo = $text;
-	$tmp = strip_tags($text);
-	// find a mention of twitpic
-	if (preg_match_all('#twitpic.com/([\d\w]+)#', $tmp, $matches, PREG_PATTERN_ORDER) > 0) {
-		foreach ($matches[1] as $match) {
-	     $text = "<a href='http://twitpic.com/{$match}'><img src='http://twitpic.com/show/large/{$match}' class='aligncenter' /></a>";
-	  	}
-		$text = $foo . $text;
-	}	
-	
-	return 	$text;
-}		
+<?php
+function showtwitpic($text){
+  $foo = $text;
+  $tmp = strip_tags($text);
+
+  // find a mention of twitpic
+  if (preg_match_all('#twitpic.com/([\d\w]+)#', $tmp, $matches, PREG_PATTERN_ORDER) > 0) {
+    foreach ($matches[1] as $match) {
+      $images .= "<a href='http://twitpic.com/{$match}'><img src='http://twitpic.com/show/large/{$match}' class='aligncenter' /></a>";
+    }
+    $text = $foo . '<center>' . $images . '</center>';
+  }
+
+  return $text;
+}
 
 add_filter('the_content', 'showtwitpic', 21);
-add_filter('the_excerpt', 'showtwitpic', 21);		
+add_filter('the_excerpt', 'showtwitpic', 21);
 ?>
